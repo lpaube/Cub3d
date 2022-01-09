acb8072ed9b1e23f4ccb9f79c2179e010933bccd
LPAube <lpaube@hotmail.com>
2022-01-09 11:44:50 -0500

    Add: win_width and tile_size fully dependant on map_width and map_height WIP: using flag -2d to launch window of 2d raycasting

--- srcs/context_2d.c
+++ srcs/context_2d.c
@@ -6,7 +6,7 @@
 /*   By: laube <louis-philippe.aube@hotmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 19:15:44 by laube             #+#    #+#             */
-/*   Updated: 2022/01/09 11:14:46 by laube            ###   ########.fr       */
+/*   Updated: 2022/01/09 11:28:54 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,32 +39,32 @@ void	put_diagnostics(t_cub2d *cub2d)
 	m = cub2d->mlx_inst;
 	p = cub2d->player;
 	mlx_string_put(cub2d->mlx_inst.mlx, cub2d->mlx_inst.win,
-		cub2d->mlx_inst.game_width + 20, 160, 0xFF0000, "Dir angle:");
+		cub2d->mlx_inst.game_width + 20, 60, 0xFF0000, "Dir angle:");
 	mlx_string_put(cub2d->mlx_inst.mlx, cub2d->mlx_inst.win,
-		cub2d->mlx_inst.game_width + 20, 180, 0xFF0000, "Dir X:");
+		cub2d->mlx_inst.game_width + 20, 80, 0xFF0000, "Dir X:");
 	mlx_string_put(cub2d->mlx_inst.mlx, cub2d->mlx_inst.win,
-		cub2d->mlx_inst.game_width + 20, 200, 0xFF0000, "Dir Y:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 240, 0xFFFF00, "Cam angle:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 260, 0xFFFF00, "FOV (deg)");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 280, 0xFFFF00, "Plane x:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 300, 0xFFFF00, "Plane y:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 340, 0x00FFFF, "Tile x:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 360, 0x00FFFF, "Tile y:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 400, 0xFF00FF, "Mouse x:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 420, 0xFF00FF, "Mouse y:");
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 160, 0xFF0000, ft_itoa(p.angle));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 180, 0xFF0000, gcvt(p.dir_x, 5, buf));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 200, 0xFF0000, gcvt(p.dir_y, 5, buf));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 240, 0xFFFF00, ft_itoa(p.angle - 90));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 260, 0xFFFF00, ft_itoa(p.fov));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 280, 0xFFFF00, gcvt(p.plane_x, 5, buf));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 300, 0xFFFF00, gcvt(p.plane_y, 5, buf));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 340, 0x00FFFF, ft_itoa(p.tile_x));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 360, 0x00FFFF, ft_itoa(p.tile_y));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 400, 0x000000, ft_itoa(cub2d->prev_mouse_x));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 420, 0x000000, ft_itoa(cub2d->prev_mouse_y));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 400, 0xFF00FF, ft_itoa(cub2d->mouse_x));
-	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 420, 0xFF00FF, ft_itoa(cub2d->mouse_y));
+		cub2d->mlx_inst.game_width + 20, 100, 0xFF0000, "Dir Y:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 140, 0xFFFF00, "Cam angle:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 160, 0xFFFF00, "FOV (deg)");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 180, 0xFFFF00, "Plane x:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 200, 0xFFFF00, "Plane y:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 240, 0x00FFFF, "Tile x:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 260, 0x00FFFF, "Tile y:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 300, 0xFF00FF, "Mouse x:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 20, 320, 0xFF00FF, "Mouse y:");
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 60, 0xFF0000, ft_itoa(p.angle));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 80, 0xFF0000, gcvt(p.dir_x, 5, buf));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 100, 0xFF0000, gcvt(p.dir_y, 5, buf));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 140, 0xFFFF00, ft_itoa(p.angle - 90));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 160, 0xFFFF00, ft_itoa(p.fov));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 180, 0xFFFF00, gcvt(p.plane_x, 5, buf));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 200, 0xFFFF00, gcvt(p.plane_y, 5, buf));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 240, 0x00FFFF, ft_itoa(p.tile_x));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 260, 0x00FFFF, ft_itoa(p.tile_y));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 300, 0x000000, ft_itoa(cub2d->prev_mouse_x));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 320, 0x000000, ft_itoa(cub2d->prev_mouse_y));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 300, 0xFF00FF, ft_itoa(cub2d->mouse_x));
+	mlx_string_put(m.mlx, m.win, cub2d->mlx_inst.game_width + 100, 320, 0xFF00FF, ft_itoa(cub2d->mouse_y));
 	cub2d->prev_mouse_x = cub2d->mouse_x;
 	cub2d->prev_mouse_y = cub2d->mouse_y;
 }
@@ -80,22 +80,11 @@ void	game_loop_2d(t_cub2d *cub2d)
 	put_diagnostics(cub2d);
 }
 
-int	set_tile_size(t_mlx mlx_inst)
-{
-	int	tile_size;
-
-	tile_size = mlx_inst.game_width / map_width;
-	if (mlx_inst.win_height / map_height < tile_size)
-		tile_size = mlx_inst.win_height / map_height;
-	return (tile_size);
-}
-
 void	context_2d(void)
 {
 	t_cub2d	cub2d;
 
 	cub2d.mlx_inst = mlx_inst_init(&cub2d);
-	//cub2d.tile_size = set_tile_size(cub2d.mlx_inst);
 	cub2d.player = init_player(&cub2d);
 	cub2d.mouse_x = 0;
 	cub2d.mouse_y = 0;
