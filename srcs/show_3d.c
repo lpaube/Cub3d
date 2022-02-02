e5446f7b0671eac50e6141c42f5fd639981b2db3
LPAube <lpaube@hotmail.com>
2022-02-02 17:53:08 -0500

    Add: rays properties for drawing in 3d

--- srcs/show_3d.c
+++ srcs/show_3d.c
@@ -6,7 +6,7 @@
 /*   By: mafortin <mafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 00:12:02 by laube             #+#    #+#             */
-/*   Updated: 2022/01/30 17:09:31 by mafortin         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:51:10 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	show_3d(t_cub2d *cub2d)
 	i = 0;
 	while (i < cub2d->ray_num)
 	{
-		printf("i: %d | len: %f | face: %c\n", i, cub2d->rays[i].len, cub2d->rays[i].face);
+		printf("i: %d | height: %d | top: %d | bottom: %d | face: %c\n", i, cub2d->rays[i].height, cub2d->rays[i].draw_top, cub2d->rays[i].draw_bottom, cub2d->rays[i].face);
 		i++;
 	}
 }
